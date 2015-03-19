d30861230508ebe34317abac7f40e34fb91b7ac1
phillip.whiters <phillip.whiters@weather.com>
2015-03-19 10:25:48 -0400

    added  from ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,9 +1,10 @@
 // This is just playing in the Snadbox
 // making changes to this file in branch4 -- 
 // making changes to this file in branch4 changed put to "Hi World" from "Hello World" 
-class HiWorld
+// making changes to this file in master changed put tnd cllass name to "Your World" from "Hello World" 
+class YourWorld
   def self.hello
-    puts "Hi World"
+    puts "Your World"
   end
 end
 
