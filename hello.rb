ae56b767f529b0bcc1a2ca4032520a1d9ef47408
phillip.whiters <phillip.whiters@weather.com>
2015-03-19 10:10:43 -0400

    Adding hello.rb file

---
+++ hello.rb
@@ -0,0 +1,10 @@
+// This is just playing in the Snadbox
+// making changes to this file in branch4 -- 
+// making changes to this file in branch4 changed put to "Hi World" from "Hello World" 
+class HiWorld
+  def self.hello
+    puts "Hi World"
+  end
+end
+
+HelloWorld.hello
