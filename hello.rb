fd8418bb449cb60a4069c0e3fd6c5c4507dba7d9
phillip.whiters <phillip.whiters@weather.com>
2015-03-19 10:22:48 -0400

    change class to MYWorld

--- hello.rb
+++ hello.rb
@@ -1,9 +1,10 @@
 // This is just playing in the Snadbox
 // making changes to this file in branch4 -- 
 // making changes to this file in branch4 changed put to "Hi World" from "Hello World" 
-class HiWorld
+// making changes to this file in change_class branch changed put to "MY World" from "Hello World" 
+class MYWorld
   def self.hello
-    puts "Hi World"
+    puts "MY World"
   end
 end
 
